c314e318a634149305745c1d7f9f66c577076871
Charlie Cline <charliecap@users.noreply.github.com>
2020-09-06 17:18:46 -0400

    .

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
+command = ""
 player_name = input('Please tell me your name... ')
 response = input("Hello, " + player_name + "... Would you like to help me? ")
 if response.lower() == "yes":
     print("Thank you so much!")
+    input(" ")
 elif response.lower() == "no":
     print("Oh, ok then...")
 else:
     print("What was that? I didn't understand")
     # how to get the initial question to pop again after a failed response
-
-
-
-
-
-
-
-
 
 
