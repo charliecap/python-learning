2413d6d9e1613bf2f17c122e137fbe7916086dfc
Charlie Cline <charlesclinesdesk@gmail.com>
2020-09-06 15:13:05 -0400

    added some test code

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 player_name = input('Please tell me your name... ')
-print("Hello, " + player_name + "... Would you like to help me?")
+initial_response = input("Hello, " + player_name + "... Would you like to help me? ")
+if initial_response.lower() == "yes":
+    print("Thank you so much!")
+elif initial_response.lower() == "no":
+    print("Oh, ok then...")
+else:
+    print("What was that? I didn't understand")
 
 
 
