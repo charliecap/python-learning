440d8b094644d3db341ab09795a4cbc7d1b30304
Charlie Cline <charliecap@users.noreply.github.com>
2020-09-06 17:28:45 -0400

    work

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 command = ""
+problem_solved = False
 player_name = input('Please tell me your name... ')
 response = input("Hello, " + player_name + "... Would you like to help me? ")
-if response.lower() == "yes":
+if "yes" in response.lower():
     print("Thank you so much!")
     input(" ")
-elif response.lower() == "no":
+elif "no" in response.lower():
     print("Oh, ok then...")
 else:
     print("What was that? I didn't understand")
     # how to get the initial question to pop again after a failed response
-
+while True:
+	command = input('> ').lower()
 
